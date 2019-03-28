77ff3f1af2eee0b502eba47c5aa83658d5364a10
Joseph Cerankowski <joecerankowski@gmail.com>
2019-03-28 08:59:20 -0400

    testing other stuff

--- server.js
+++ server.js
@@ -1,33 +1,41 @@
-var express = require("express");
-var logger = require("morgan");
-var mongoose = require("mongoose");
-var axios = require("axios");
-var cheerio = require("cheerio");
+// Dependencies
+const express = require("express");
+const bodyParser = require("body-parser"); //JSON responses
+const mongoose = require("mongoose"); //Mongo object modelling 
+const request = require("request"); //Makes http calls
+const cheerio = require("cheerio"); //Scraper
 
 // Require all models
-var db = require("./models");
+const db = require("./models");
 
-var PORT = 3000;
+// Port configuration for local/Heroku
+const PORT = process.env.PORT || process.argv[2] || 8080;
 
 // Initialize Express
-var app = express();
+const app = express();
 
-// Configure middleware
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Use morgan logger for logging requests
-app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Make public a static folder
+// Handlebars
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
-
+// Controllers
+const router = require("./controllers/api.js");
+app.use(router);
 // Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
 // Start the server
-app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
+app.listen(PORT, function () {
+    console.log(`This application is running on port: ${PORT}`);
 });
