ec29b824ceb6be0636877fed66f4af12129e8083
Joseph Cerankowski <joecerankowski@gmail.com>
2019-04-01 12:39:30 -0400

    finally working right

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -33,7 +33,8 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 //mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
-
+var MONGODB_URI = process.env.MONGODB_URI|| "mongodb://localhost/unit18Populater";
+mongoose.connect(MONGODB_URI);
 
 
 
